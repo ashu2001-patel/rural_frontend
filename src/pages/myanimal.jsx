9ab6cde40342fd1b034b4ce17be664f491b7c102
Ashirvad Patel <ashirvad@gmail.com>
2026-04-15 06:04:26 +0100

    added the some extra feature in the frontend like my lisining and plant

--- src/pages/myanimal.jsx
+++ src/pages/myanimal.jsx
@@ -23,7 +23,7 @@ const MyAnimals = () => {
     try {
       setLoading(true);
       // ✅ Fixed endpoint
-      const res = await animalAPI.get(`/animal/employer/${user.id}`);
+      const res = await animalAPI.get(`/my`);
       setAnimals(res.data.animals || []);
     } catch (err) {
       console.error(err);
